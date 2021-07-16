d16913d2bd27c0d20090c8e2f3afdf7c21c8ae65
NiltonLuan <nilton.luan.guedes08@aluno.ifce.edu.br>
2021-07-15 22:54:59 -0300

    Atualizando codes

---
+++ Atividades-Avaliacao-Etapa2/ATV07/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from controller import clientes
+from controller import produtos
+from controller import notas_fiscais
+from controller import itens_notafiscal
+from ATV07.model import tabelas
